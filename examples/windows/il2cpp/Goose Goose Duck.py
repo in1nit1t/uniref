ec325e24af341257dd3ef57e085a3147da1884a2
in1nit1t <gfkdlyk@163.com>
2023-02-09 23:37:29 +0800

    Update offsets

--- examples/windows/il2cpp/Goose Goose Duck.py
+++ examples/windows/il2cpp/Goose Goose Duck.py
@@ -3,7 +3,23 @@ from uniref import WinUniRef
 # A game on Steam
 # WARNING: Due to the frequent updates of the game, the script may fail to run.
 
-# 1. Run game & Start a tutorial
+"""
+To avoid EAC loading, you need to delete all files except Settings.json in the EasyAntiCheat folder
+under the game root directory (please make a backup).
+Then replace the contents of the file with the following.
+
+{
+	"title"											: "Goose Goose Duck",
+	"executable"									: "Goose Goose Duck.exe",
+	"productid"										: "75ae519171904d40b767b1547dd91c37f",
+	"sandboxid"										: "a85f25c87846431a8adbe9246701f32df",
+	"deploymentid"									: "d569cc9d3e0945e9b7e5bb508c3dbc73f",
+	"requested_splash"								: "EasyAntiCheat/SplashScreen.png",
+	"wait_for_game_process_exit"					: "true"
+}
+"""
+
+# 1. Run game in steam & Start a tutorial
 # 2. Run this script
 # 3. Function modify_speed will give you a higher speed
 # 4. Function show_my_position will print your (x, y, z) position
@@ -17,9 +33,12 @@ def modify_speed(ref: WinUniRef):
     movementSpeed_address = static_field + movementSpeed.offset
 
     # fight against anti-cheating mechanisms
+    # check https://github.com/Liuhaixv/Goose_Goose_Duck_Hack/pull/259 for more details
     ref.injector.mem_write_bool(movementSpeed_address + 0x14, False)
     ref.injector.mem_write_float(movementSpeed_address, 20.0)
     ref.injector.mem_write_uint32(movementSpeed_address + 4, 0)
+
+    print("Now you cam move fast")
 
 
 def show_my_position(ref: WinUniRef):
@@ -28,7 +47,7 @@ def show_my_position(ref: WinUniRef):
 
     LocalPlayer = ref.find_class_in_image("Assembly-CSharp.dll", "Handlers.GameHandlers.PlayerHandlers.LocalPlayer")
     try:
-        local_player_instance = ref.injector.mem_read_multilevel_pointer(game_assembly_base, [0x3CDB720, 0xB8, 0])
+        local_player_instance = ref.injector.mem_read_multilevel_pointer(game_assembly_base, [0x4D19958, 0xB8, 0])
     except:
         print(error_hint)
         exit(-1)
@@ -47,7 +66,7 @@ def show_my_position(ref: WinUniRef):
     for i in range(3):
         pos_value.append(ref.injector.mem_read_float(position.address + i * 4))
     x, y, z = pos_value
-    print("position: ", x, y, z)
+    print("Position: ", x, y, z)
 
 
 if __name__ == "__main__":
