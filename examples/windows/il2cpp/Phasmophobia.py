e74f47fdf3adbc8ae033a7bfd314dea82583fa17
in1nit1t <in1t@outlook.com>
2023-04-02 00:34:51 +0800

    Optimize `Phasmophobia` example code

--- examples/windows/il2cpp/Phasmophobia.py
+++ examples/windows/il2cpp/Phasmophobia.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+from os import system
+from time import sleep
 from uniref import WinUniRef
 
 # A game on Steam. This script shows the ghost room & ghost type.
@@ -13,16 +14,21 @@ from uniref import WinUniRef
 ghost_type_str = ["Spirit", "Wraith", "Phantom", "Poltergeist", "Banshee", "Jinn", "Mare", "Revenant", "Shade", "Demon", "Yurei", "Oni", "Yokai", "Hantu", "Goryo", "Myling", "Onryo", "TheTwins", "Raiju", "Obake", "Mimic", "Moroi", "Deogen", "Thaye"]
 
 
+def get_room_str(level_room):
+    for field in level_room.list_fields():
+        if field.type_name == "System.String":
+            return field.value
+
+
 ref = WinUniRef("Phasmophobia.exe")
 
 lc = ref.find_class_in_image("Assembly-CSharp.dll", "LevelController")
 ai = ref.find_class_in_image("Assembly-CSharp.dll", "GhostAI")
 info = ref.find_class_in_image("Assembly-CSharp.dll", "GhostInfo")
 lr = ref.find_class_in_image("Assembly-CSharp.dll", "LevelRoom")
+ls = ref.find_class_in_image("Assembly-CSharp.dll", "LevelStats")
 
-for field in lc.list_fields():
-    if field.is_static() and field.offset == 0:
-        lc.instance = field.value
+lc.instance = lc.find_field_by_offset(0).value
 
 for field in lc.list_fields():
     if field.type_name == "GhostAI":
@@ -32,13 +38,30 @@ for field in ai.list_fields():
     if field.type_name == "GhostInfo":
         info.instance = field.value
 
-for field in info.list_fields():
-    if field.type_name == "LevelRoom":
-        lr.instance = field.value
-
-for field in lr.list_fields():
-    if field.type_name == "System.String":
-        print("Ghost Room:", field.value)
-
 ghost_type = info.find_field_by_offset(0x20)
-print("Ghost Type:", ghost_type_str[ghost_type.value & 0xFF])
+ghost_type = ghost_type_str[ghost_type.value & 0xFF]
+
+ls.instance = ls.find_field("_instance").value
+bone_room = ls.find_field("boneRoom").value
+
+my_room = lc.find_field_by_offset(0x28)
+ghost_room = lc.find_field_by_offset(0x30)
+
+
+try:
+    while True:
+        system("cls")
+
+        lr.instance = ghost_room.value
+        print("Ghost Room:", get_room_str(lr))
+
+        print("Bone Room:", bone_room)
+
+        print("Ghost Type:", ghost_type)
+
+        lr.instance = my_room.value
+        print("My Position:", get_room_str(lr))
+
+        sleep(1)
+except KeyboardInterrupt:
+    exit(0)
